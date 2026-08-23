3faab7dd11855d4c38215a1ff8df3aabf6844f33
Rafatuba <rafaolly1@gmail.com>
2026-08-22 22:15:52 -0400

    Implementação das configurações de PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Guia de Viagem",
+        short_name: "Guia de Viagem",
+        description: "Meu guia de viagem de Campo Grande para Goiânia",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/iconcar.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/iconcar.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
