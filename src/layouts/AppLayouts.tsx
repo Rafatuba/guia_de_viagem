e134e6402e1b6ccc49b6739ccfd3c7e46f685a0d
Rafatuba <rafaolly1@gmail.com>
2026-08-26 13:37:22 -0400

    Implementação do ScrollToTop

--- src/layouts/AppLayouts.tsx
+++ src/layouts/AppLayouts.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import MenuNav from "../components/MenuNav";
 import Header from "../components/Header";
+import ScrollToTop from "../components/ParaTopo";
 
 export default function AppLayout() {
   return (
     <div className="flex flex-col min-h-screen ml-3 mr-3 mt-3">
+      <ScrollToTop />
       <Header />
       <main>
         <Outlet />
